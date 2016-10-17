d7dfa3e4a16145c4a4af119660df9a3d8fc53c13
sherri010 <aminshahidi.sh@gmail.com>
2016-10-17 12:18:27 -0700

    completes posting a new record

--- record_store/app/controllers/records_controller.rb
+++ record_store/app/controllers/records_controller.rb
@@ -5,5 +5,17 @@ class RecordsController < ApplicationController
   def index
     @record = Record.all
   end
+  def new
+     @record = Record.new
+     render :new #optional
+  end
+    def create
+    Record.create(record_params)
+    redirect_to('/records')
+  end
+  private
 
+  def record_params
+    params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+  end
 end
--- record_store/config/routes.rb
+++ record_store/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/records/:id", to: "records#show"
-  get "/records", to: "records#index"
+get "/records" => "records#index", as: 'records'
+get "/records/new" => "records#new", as: 'new_record'
+get "/records/:id" => "records#show", as: 'record'
+post "/records" => "records#create"
 end
